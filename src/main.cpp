30788d692430223c1fda974eaafbe06b505adc4e
ngrasset <ngrasset@student.42.fr>
2018-02-26 14:34:18 +0100

    accept ;; as a token from file

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,13 @@ static void trim(std::string& s) {
 		s.erase(p + 1);
 }
 
-std::vector<std::string> readFile(std::istream &stream) {
+std::vector<std::string> readFile(std::istream &stream, bool isStdin) {
 	std::vector<std::string> lines;
 	std::string line;
 
 	while (std::getline(stream, line)) {
 		trim(line);
-		if (line == ";;")
+		if (isStdin == true && line == ";;")
 			break;
 		if (line.length() > 0) {
 			lines.push_back(line);
@@ -36,12 +36,12 @@ int main(int ac, char** av) {
 	try {
 		std::vector<std::string> source;
 		if (ac <= 1) {
-			source = readFile(std::cin);
+			source = readFile(std::cin, true);
 		} else {
 			std::ifstream file(av[1]);
 			if (!file.is_open())
 				throw Exception("Unable to open file " + std::string(av[1]));
-			source = readFile(file);
+			source = readFile(file, false);
 			file.close();
 		}
 		auto instructions = parseFile(source);
